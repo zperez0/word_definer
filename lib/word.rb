1467d590361526d4fe12117aa6be42b3ec026f58
Yazmin Perez <zoeperez333@gmail.com>
2022-05-05 15:04:21 -0700

    add initalize and save method. update spec, test 2 good fail

--- lib/word.rb
+++ lib/word.rb
@@ -2,8 +2,14 @@ class Word
 
   @@word = {}
 
+  def initialize(str, id)
+  end
+
   def self.all()
     @@word.values()
+  end
 
+  def save()
+  
   end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -7,4 +7,14 @@ describe('Word') do
     expect(Word.all).to(eq([]))
     end
   end
+
+  describe('#save') do
+    it("saves a word") do
+      word = Word.new("earth bender", nil)
+      word.save()
+      word2 = Word.new("water bender", nil)
+      word2.save()
+      expect(Word.all).to(eq([word, word2]))
+    end
+  end
 end
