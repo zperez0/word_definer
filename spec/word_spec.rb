a66722cb7d787ca719563a9db282fc1b1bc0db14
Yazmin Perez <zoeperez333@gmail.com>
2022-05-05 16:34:52 -0700

    test 4 good fail

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -4,12 +4,6 @@ require('word')
 describe('Word') do
 
 
-  describe('.all') do
-    it("returns an empty array when there are no words") do
-    expect(Word.all).to(eq([]))
-    end
-  end
-
   describe('#save') do
     it("saves a word") do
       word = Word.new("earth bender", nil)
@@ -20,11 +14,28 @@ describe('Word') do
     end
   end
 
+  describe('.all') do
+    it("returns an empty array when there are no words") do
+    expect(Word.all).to(eq([]))
+    end
+  end
+
   describe('#==') do
     it("is the same word if it has the same attributes as another word") do
     word = Word.new("earth bender", nil)
     word2 = Word.new("earth bender", nil)
     expect(word).to(eq(word2))
+    end
+  end
+
+  describe('.clear') do
+    it("clears all words") do
+      word = Word.new("flying bison", nil)
+      word.save()
+      word2 = Word.new("fire bender", nil)
+      word2.save()
+      Word.clear()
+      expect(Word.all).to(eq([]))
     end
   end
 end
