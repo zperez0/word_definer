acff93e46828e55e7cc778925e5f85ee38197707
Yazmin Perez <zoeperez333@gmail.com>
2022-05-05 21:10:55 -0700

    update Define class. test 1-7 passing

--- lib/definition.rb
+++ lib/definition.rb
@@ -11,5 +11,33 @@ class Define
     @id = id || @@total_rows += 1
   end
 
+  def ==(def_to_compare)
+    (self.str() == def_to_compare.str()) && (self.word_id() == def_to_compare.word_id())
+  end
 
+  def self.all
+    @@defs.values
+  end
+
+  def save
+    @@defs[self.id] = Define.new(self.str, self.word_id, self.id)
+  end
+
+  def self.find(id)
+    @@defs[id]
+  end
+
+  def update(str, word_id)
+    self.str = str
+    self.word_id = word_id
+    @@defs[self.id] = Define.new(self.str, self.word_id, self.id)
+  end
+
+  def delete
+    @@defs.delete(self.id)
+  end
+
+  def self.clear
+    @@defs = {}
+  end
 end
