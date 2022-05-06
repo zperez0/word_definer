5c6d33ebfb763180ba9d7809a6f775ea43d30b5b
Yazmin Perez <zoeperez333@gmail.com>
2022-05-06 11:58:25 -0700

    add test 10, good fail

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -14,28 +14,28 @@ describe('Define') do
 
   describe('#==') do
     it("is the same definition if it has the same attributes as another definition") do
-      definition = Define.new("homie", @word.id, nil)
-      definition2 = Define.new("homie", @word.id, nil)
-      expect(definition).to(eq(definition2))
+      def1 = Define.new("homie", @word.id, nil)
+      def2 = Define.new("homie", @word.id, nil)
+      expect(def1).to(eq(def2))
     end
   end
 
   describe('.all') do
     it("returns an array of definitions") do
-      definition = Define.new("flying bison", @word.id, nil)
-      definition.save()
-      definition2 = Define.new("homie for life", @word.id, nil)
-      definition2.save()
-      expect(Define.all).to(eq([definition, definition2]))
+      def1 = Define.new("flying bison", @word.id, nil)
+      def1.save()
+      def2 = Define.new("homie for life", @word.id, nil)
+      def2.save()
+      expect(Define.all).to(eq([def1, def2]))
     end
   end
 
   describe('.clear') do
     it("clears all definitions") do
-      definition = Define.new("air bison", @word.id, nil)
-      definition.save()
-      definition2 = Define.new("homie for life", @word.id, nil)
-      definition2.save()
+      def1 = Define.new("air bison", @word.id, nil)
+      def1.save()
+      def2 = Define.new("homie for life", @word.id, nil)
+      def2.save()
       Define.clear()
       expect(Define.all).to(eq([]))
     end
@@ -43,39 +43,39 @@ describe('Define') do
 
   describe('#save') do
     it("saves a definition") do
-      definition = Define.new("homie for life", @word.id, nil)
-      definition.save()
-      expect(Define.all).to(eq([definition]))
+      def1 = Define.new("homie for life", @word.id, nil)
+      def1.save()
+      expect(Define.all).to(eq([def1]))
     end
   end
 
   describe('.find') do
     it("finds a definition by id") do
-      definition = Define.new("homie for life", @word.id, nil)
-      definition.save()
-      definition2 = Define.new("air bison", @word.id, nil)
-      definition2.save()
-      expect(Define.find(definition.id)).to(eq(definition))
+      def1 = Define.new("homie for life", @word.id, nil)
+      def1.save()
+      def2 = Define.new("air bison", @word.id, nil)
+      def2.save()
+      expect(Define.find(def1.id)).to(eq(def1))
     end
   end
 
   describe('#update') do
     it("updates a definition by id") do
-      definition = Define.new("air bison", @word.id, nil)
-      definition.save()
-      definition.update("sky bison", @word.id)
-      expect(definition.str).to(eq("sky bison"))
+      def1 = Define.new("air bison", @word.id, nil)
+      def1.save()
+      def1.update("sky bison", @word.id)
+      expect(def1.str).to(eq("sky bison"))
     end
   end
 
   describe('#delete') do
     it("deletes a definition by id") do
-      definition = Define.new("air bison", @word.id, nil)
-      definition.save()
-      definition2 = Define.new("homie for life", @word.id, nil)
-      definition2.save()
-      definition.delete()
-      expect(Define.all).to(eq([definition2]))
+      def1 = Define.new("air bison", @word.id, nil)
+      def1.save()
+      def2 = Define.new("homie for life", @word.id, nil)
+      def2.save()
+      def1.delete()
+      expect(Define.all).to(eq([def2]))
     end
   end
 
@@ -83,11 +83,19 @@ describe('Define') do
     it("finds a definition for a word") do
       word2 = Word.new("air bender", nil)
       word2.save()
-      definition = Define.new("homie for life", @word.id, nil)
-      definition.save()
-      definition2 = Define.new("one who can control air", word2.id, nil)
-      definition2.save()
-      expect(Define.find_by_word(word2.id)).to(eq([definition2]))
+      def1 = Define.new("homie for life", @word.id, nil)
+      def1.save()
+      def2 = Define.new("one who can control air", word2.id, nil)
+      def2.save()
+      expect(Define.find_by_word(word2.id)).to(eq([def2]))
+    end
+  end
+
+  describe('#word') do
+    it("finds the word a definition belongs to") do
+      def1 = Define.new("homie for life", @word.id, nil)
+      def1.save()
+      expect(def1.word()).to(eq(@word))
     end
   end
 end
