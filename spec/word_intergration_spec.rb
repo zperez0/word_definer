0ce184899b69fe08df72a5dc8201e5fea6561d6f
Yazmin Perez <zoeperez333@gmail.com>
2022-05-06 20:02:17 -0700

    add test 5-6. tests are passing

--- spec/word_intergration_spec.rb
+++ spec/word_intergration_spec.rb
@@ -1,60 +1,73 @@
-require('capybara/rspec')
-require('./app')
+require("capybara/rspec")
+require("./app")
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe('create a word path', {:type => :feature}) do
-  it('creates a word and then goes to the word page') do
-    visit('/words')
-    click_on('Add a new word')
-    fill_in('word_str', :with => 'flying bison')
-    click_on('Add')
-    expect(page).to(have_content('flying bison'))
+describe("create a word path", { :type => :feature }) do
+  it("creates a word and then goes to the word page") do
+    visit("/words")
+    click_on("Add a new word")
+    fill_in("word_str", :with => "flying bison")
+    click_on("Add")
+    expect(page).to(have_content("flying bison"))
   end
 end
 
-describe('create a definition path', {:type => :feature}) do
-  it('creates a word, definition and then displays definition') do
-    word = Word.new('flying bison', nil)
+describe("create a definition path", { :type => :feature }) do
+  it("creates a word, definition and then displays definition") do
+    word = Word.new("flying bison", nil)
     word.save()
     visit("/words/#{word.id}")
-    fill_in('def_str', :with => 'homie')
-    click_on('Add')
-    expect(page).to(have_content('homie'))
+    fill_in("def_str", :with => "homie")
+    click_on("Add")
+    expect(page).to(have_content("homie"))
   end
 end
 
-# edit/update word
-describe('edit a word', {:type => :feature}) do
-  it('edit a word and return to word page') do
-    word = Word.new('flying bison', nil)
+describe("edit a word", { :type => :feature }) do
+  it("edit a word and return to word page") do
+    word = Word.new("flying bison", nil)
     word.save()
     visit("/words/#{word.id}")
-    click_on('Edit word')
-    fill_in('str', :with => 'appa')
-    click_on('Update')
-    expect(page).to(have_content('appa'))
+    click_on("Edit word")
+    fill_in("str", :with => "appa")
+    click_on("Update")
+    expect(page).to(have_content("appa"))
   end
 end
 
-# edit/update def
-describe('edit a definition', {:type => :feature}) do
-  it('edit a definition and return to definition page') do
-  word = Word.new('flying bison', nil)
-  word.save()
-  def1 = Define.new('homie for life', word.id, nil)
-  def1.save()
-  visit("/words/#{word.id}/definitions/#{def1.id}")
-  fill_in('str', :with => 'turtle duck')
-  click_on('Update')
-  expect(page).to(have_content('turtle duck'))
+describe("edit a definition", { :type => :feature }) do
+  it("edit a definition and return to word page") do
+    word = Word.new("flying bison", nil)
+    word.save()
+    def1 = Define.new("homie for life", word.id, nil)
+    def1.save()
+    visit("/words/#{word.id}/definitions/#{def1.id}")
+    fill_in("str", :with => "turtle duck")
+    click_on("Update")
+    expect(page).to(have_content("turtle duck"))
   end
 end
 
+describe("delete a word", { :type => :feature }) do
+  it("delete a word and return to word page") do
+    word = Word.new("flying bison", nil)
+    word.save()
+    visit("/words/#{word.id}")
+    click_on("Edit word")
+    click_on("Delete")
+    expect(page).to(have_content(""))
+  end
+end
 
-# delete word
-
-
-# delete def
-
-# return to word list?
+describe("delete a definition", { :type => :feature }) do
+  it("delete a definition and return to word page") do
+    word = Word.new("flying bison", nil)
+    word.save()
+    def1 = Define.new("homie for life", word.id, nil)
+    def1.save()
+    visit("/words/#{word.id}/definitions/#{def1.id}")
+    click_on("Delete")
+    expect(page).to(have_content(""))
+  end
+end
